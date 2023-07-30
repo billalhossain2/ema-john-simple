3218f03335d4f18bca4e1d0d4e42c883fe76d942
billal-webdev <billal-webdev@gmail.com>
2023-07-30 12:11:48 +0600

    basic folder structure set up

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+        <h1>App</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste sapiente facere minus excepturi ipsam, eveniet repellendus nisi blanditiis at libero itaque iure illo hic inventore? Recusandae eveniet vitae nobis, laborum corrupti magnam ex incidunt asperiores, ipsam veniam fugit vero non cum aliquam. Mollitia eum ratione voluptas similique. Minus, dicta adipisci.
+        </p>
+    </div>
+  )
+}
+
+export default App
